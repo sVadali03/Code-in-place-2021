b33f47db12084fcdd15f02c6fd4212bcddb4c82b
Supriya Vadali <66241504+sVadali03@users.noreply.github.com>
2021-05-14 16:31:09 +0530

    Add files via upload

---
+++ Assignment 3 Images/Finding Forest Flames/forest_fire.py
@@ -0,0 +1,58 @@
+"""
+This program highlights fires in an image by identifying pixels
+whose red intensity is more than INTENSITY_THRESHOLD times the
+average of the red, green, and blue values at a pixel. Those
+"sufficiently red" pixels are then highlighted in the image
+and other pixels are turned grey, by setting the pixel red,
+green, and blue values to be all the same average value.
+"""
+
+from simpleimage import SimpleImage
+
+DEFAULT_FILE = 'images/greenland-fire.png'
+
+def find_flames(filename):
+    """
+    This function should highlight the "sufficiently red" pixels
+    in the image and grayscale all other pixels in the image
+    in order to highlight areas of wildfires.
+    """
+    image = SimpleImage(filename)
+    for pixel in image:
+        x = pixel.red
+        y = pixel.green
+        z = pixel.blue
+        avg = (x+y+z)/3
+        if pixel.red > avg:
+            pixel.red = 225
+            pixel.green = 0
+            pixel.blue = 0
+        else:
+            pixel.red = avg
+            pixel.green = avg
+            pixel.blue = avg
+    return image
+
+def main():
+    # Get file and load image
+    filename = get_file()
+    image = SimpleImage(filename)
+
+    # Show the original fire
+    original_fire = SimpleImage(filename)
+    original_fire.show()
+
+    # Show the highlighted fire
+    highlighted_fire = find_flames(filename)
+    highlighted_fire.show()
+
+def get_file():
+    # Read image file path from user, or use the default file
+    filename = input('Enter image file (or press enter): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+
+if __name__ == '__main__':
+    main()
